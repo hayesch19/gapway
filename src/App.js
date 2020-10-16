f29c8659121c7bb62d0c3f86b8cc76db7c369814
hayesch19 <chrishayespro@gmail.com>
2020-10-16 08:30:28 -0400

    Added Routing for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import HeaderNav from './components/HeaderNav'
 import HomePage from './pages/HomePage'
+import ServiceTimes from './pages/ServiceTimes'
+import SermonsPage from './pages/SermonsPage'
+import SpecialEvents from './pages/SpecialEvents'
+import AboutUs from './pages/AboutUs'
+import FooterBar from './components/FooterBar'
 
 class App extends Component {
   render() {
-    return <HomePage />
+    return (
+      <Router>
+        <HeaderNav />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route exact path="/" component={ServiceTimes}></Route>
+          <Route exact path="/" component={SermonsPage}></Route>
+          <Route exact path="/" component={SpecialEvents}></Route>
+          <Route exact path="/" component={AboutUs}></Route>
+        </Switch>
+        <FooterBar />
+      </Router>
+    )
   }
 }
 
